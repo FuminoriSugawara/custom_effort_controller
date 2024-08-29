49af3c7363094ad6d786481e7427ef8e83810b84
FuminoriSugawara <fuminori.sugawara@gmail.com>
2024-08-29 01:30:59 +0000

    Add CustomEffortController class with state interface configuration and update method

--- include/custom_effort_controller/custom_effort_controller.hpp
+++ include/custom_effort_controller/custom_effort_controller.hpp
@@ -12,14 +12,35 @@
 namespace custom_effort_controller
 {
 
-class CustomEffortController : public effort_controllers::JointGroupEffortController
-{
+    class CustomEffortController : public effort_controllers::JointGroupEffortController
+    {
     public:
-    controller_interface::return_type update(const rclcpp::Time & time, const rclcpp::Duration & period) override;
- 
+        CustomEffortController() = default;
 
-};
+        controller_interface::CallbackReturn on_init() override;
+        controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
+        controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+        controller_interface::CallbackReturn on_activate(
+            const rclcpp_lifecycle::State &previous_state) override;
+        controller_interface::CallbackReturn on_deactivate(
+            const rclcpp_lifecycle::State &previous_state) override;
+        controller_interface::return_type update(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
-}  // namespace custom_effort_controller
+    private:
+        std::vector<std::string> joint_names_;
+        std::vector<std::string> state_interface_types_;
+        std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> joint_position_state_interface_;
+        std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> joint_velocity_state_interface_;
+        std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> joint_effort_state_interface_;
+        std::unordered_map<
+            std::string, std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> *>
+            state_interface_map_ = {
+                {"position", &joint_position_state_interface_},
+                {"velocity", &joint_velocity_state_interface_},
+                {"effort", &joint_effort_state_interface_},
+                };
+    };
 
-#endif  // CUSTOM_EFFORT_CONTROLLER_HPP_
+} // namespace custom_effort_controller
+
+#endif // CUSTOM_EFFORT_CONTROLLER_HPP_
